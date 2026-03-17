764f396ac6dabee3a1218aed65369ece3d809191
NhatBarbie001 <LDnhat001@gmail.com>
2026-03-17 19:11:26 +0700

    ..

--- continual_clip/models.py
+++ continual_clip/models.py
@@ -19,7 +19,7 @@ import numpy as np
 
 
 def forward_clip(self, image, text, return_feature=False, _cur_task: int = -1):
-    print("=======================forward clip ======================")
+    print("=======================2. forward_clip ======================")
     print(_cur_task)
     image_features = self.encode_image(image, _cur_task=_cur_task)
     text_features = self.encode_text(text)
@@ -120,7 +120,7 @@ class ClassIncrementalCLIP(nn.Module):
         return logits_per_image
 
     def forward(self, image, _cur_task:int = -1, test=False, all_test=False, return_feature=False,replay=None):
-        print("======================= forward ClassincrementalCLIP ======================")
+        print("=======================1. forward ClassincrementalCLIP ======================")
         if test:
             # pdb.set_trace()
             with torch.no_grad():
